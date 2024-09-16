42c5579496b6b4509d761fbff15c616d3d6ae9b3
Dalton Bohning <dalton.bohning@intel.com>
2024-09-16 18:32:32 +0000

    DAOS-623 test: Support running independent io sys admin steps
    Support running independent io sys admin steps from the yaml.
    
    Skip-unit-tests: true
    Skip-fault-injection-test: true
    
    Required-githooks: true
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/deployment/io_sys_admin.py
+++ src/tests/ftest/deployment/io_sys_admin.py
@@ -40,66 +40,87 @@ class IoSysAdmin(DataMoverTestBase, FileCountTestBase):
         new_cont_user = self.params.get("user", "/run/container_set_owner/*")
         new_cont_group = self.params.get("group", "/run/container_set_owner/*")
 
+        # Toggle independent steps
+        run_step_pool_create_ownership = self.params.get(
+            "run_step_pool_create_ownership", "/run/io_sys_admin/*", True)
+        run_step_storage_system_query = self.params.get(
+            "run_step_storage_system_query", "/run/io_sys_admin/*", True)
+        run_step_io = self.params.get(
+            "run_step_io", "/run/io_sys_admin/*", True)
+        run_step_snapshot = self.params.get(
+            "run_step_snapshot", "/run/io_sys_admin/*", True)
+        run_test_datamover = self.params.get(
+            "run_test_datamover", "/run/io_sys_admin/*", True)
+
         dmg = self.get_dmg_command()
         daos = self.get_daos_command()
 
-        for idx in range(1, 4):
-            pool = self.get_pool(namespace=f"/run/pool_{idx}/", create=False)
-            check_pool_creation(self, [pool], 60)
-            containers = []
-            for cont_idx in range(1, 4):
-                containers.append(
-                    self.get_container(pool, namespace=f"/run/container_{cont_idx}/"))
-                containers[-1].set_owner(f"{new_cont_user}@", f"{new_cont_group}@")
+        if run_step_pool_create_ownership:
+            self.log_step("Verify pool creation time and container set-owner")
+            for idx in range(1, 4):
+                pool = self.get_pool(namespace=f"/run/pool_{idx}/", create=False)
+                check_pool_creation(self, [pool], 60)
+                containers = []
+                for cont_idx in range(1, 4):
+                    containers.append(
+                        self.get_container(pool, namespace=f"/run/container_{cont_idx}/"))
+                    containers[-1].set_owner(f"{new_cont_user}@", f"{new_cont_group}@")
 
-            daos.container_list(pool.identifier)
-            self.destroy_containers(containers)
-            pool.destroy()
+                daos.container_list(pool.identifier)
+                self.destroy_containers(containers)
+                pool.destroy()
 
-        # dmg storage scan
-        dmg.storage_scan()
-        dmg.system_query()
-        dmg.system_leader_query()
+        if run_step_storage_system_query:
+            self.log_step("Verify storage scan and system query")
+            dmg.storage_scan()
+            dmg.system_query()
+            dmg.system_leader_query()
 
-        # write large data sets
-        self.run_file_count()
-        # create snapshot
-        self.container[-1].create_snap()
-        # overwrite the last ior file
-        self.ior_cmd.signature.update('456')
-        self.processes = self.ior_np
-        self.ppn = self.ior_ppn
-        self.run_ior_with_pool(create_pool=False, create_cont=False)
+        if run_step_io:
+            self.log_step("Verifying large dataset IO")
+            self.run_file_count()
 
-        nvme_free_space_before_snap_destroy = self.get_free_space()[1]
-        # delete snapshot
-        self.container[-1].destroy_snap(epc=self.container[-1].epoch)
-        # Now check if the space is returned back.
-        counter = 1
-        returned_space = self.get_free_space()[1] - nvme_free_space_before_snap_destroy
+        if run_step_snapshot:
+            self.log_step("Verifying snapshot creation and aggregation")
+            self.container[-1].create_snap()
+            # overwrite the last ior file
+            self.ior_cmd.signature.update('456')
+            self.processes = self.ior_np
+            self.ppn = self.ior_ppn
+            self.run_ior_with_pool(create_pool=False, create_cont=False)
 
-        data_written = (int(self.ppn) * human_to_bytes(self.ior_cmd.block_size.value))
-        while returned_space < int(data_written):
-            # try to wait for 4 x 60 secs for aggregation to be completed or
-            # else exit the test with a failure.
-            if counter > 4:
-                self.log.info("Free space before snapshot destroy: %s",
-                              nvme_free_space_before_snap_destroy)
-                self.log.info("Free space when test terminated: %s",
-                              self.get_free_space()[1])
-                self.fail("Aggregation did not complete as expected")
-
-            time.sleep(60)
+            nvme_free_space_before_snap_destroy = self.get_free_space()[1]
+            # delete snapshot
+            self.container[-1].destroy_snap(epc=self.container[-1].epoch)
+            # Now check if the space is returned back.
+            counter = 1
             returned_space = self.get_free_space()[1] - nvme_free_space_before_snap_destroy
-            counter += 1
 
-        self.log.info("#####Starting FS_COPY Test")
-        self.run_dm_activities_with_ior("FS_COPY", self.pool, self.container[-1])
-        self.log.info("#####Starting DCP Test")
-        self.run_dm_activities_with_ior("DCP", self.pool, self.container[-1])
-        self.log.info("#####Starting DSERIAL Test")
-        self.run_dm_activities_with_ior("DSERIAL", self.pool, self.container[-1])
-        self.log.info("#####Starting CONT_CLONE Test")
-        self.run_dm_activities_with_ior("CONT_CLONE", self.pool, self.container[-1])
-        self.log.info("#####Completed all Datamover tests")
-        self.container.pop(0)
+            data_written = (int(self.ppn) * human_to_bytes(self.ior_cmd.block_size.value))
+            while returned_space < int(data_written):
+                # try to wait for 4 x 60 secs for aggregation to be completed or
+                # else exit the test with a failure.
+                if counter > 4:
+                    self.log.info(
+                        "Free space before snapshot destroy: %s",
+                        nvme_free_space_before_snap_destroy)
+                    self.log.info(
+                        "Free space when test terminated: %s", self.get_free_space()[1])
+                    self.fail("Aggregation did not complete as expected")
+
+                time.sleep(60)
+                returned_space = self.get_free_space()[1] - nvme_free_space_before_snap_destroy
+                counter += 1
+
+        if run_test_datamover:
+            self.log_step("Verifying datamover")
+            self.log.info("#####Starting FS_COPY Test")
+            self.run_dm_activities_with_ior("FS_COPY", self.pool, self.container[-1])
+            self.log.info("#####Starting DCP Test")
+            self.run_dm_activities_with_ior("DCP", self.pool, self.container[-1])
+            self.log.info("#####Starting DSERIAL Test")
+            self.run_dm_activities_with_ior("DSERIAL", self.pool, self.container[-1])
+            self.log.info("#####Starting CONT_CLONE Test")
+            self.run_dm_activities_with_ior("CONT_CLONE", self.pool, self.container[-1])
+            self.log.info("#####Completed all Datamover tests")
+            self.container.pop(0)
